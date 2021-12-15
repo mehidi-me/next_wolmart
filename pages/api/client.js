5fe083b72237aa6867d2c2246834d7c23275e288
Mehidi Hasan <mehidi@macdeMacBook-Pro.local>
2021-12-15 14:08:23 +0600

    bug fix

--- pages/api/client.js
+++ pages/api/client.js
@@ -1,5 +1,5 @@
-export default "http://206.189.141.186/api/v2/";
+export default "//206.189.141.186/api/v2/";
 
-const imgPath = "http://206.189.141.186/public/";
+const imgPath = "//206.189.141.186/public/";
 
 export { imgPath };
