4f1c6175d5cb82241913a18b08592a1df51bc5aa
Mehidi Hasan <mehidi@macdeMacBook-Pro.local>
2021-12-30 15:03:33 +0600

    bug fix

--- component/Banner.jsx
+++ component/Banner.jsx
@@ -24,7 +24,7 @@ export default function Banner() {
           setSliders(data.data);
         }
       });
-  });
+  }, []);
   return (
     <section className="intro-section banner">
       <Swiper
